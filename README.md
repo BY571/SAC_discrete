b426178ad5f63b5d89d9725e2d08064f04012c6b
BY571 <sebastian.dittert@gmx.de>
2021-09-30 21:34:51 +0200

    add discrete sac and CartPole results

---
+++ README.md
@@ -0,0 +1,8 @@
+# SAC Discrete
+
+PyTorch implementation of the discrete Soft-Actor-Critic (SAC) algorithm.
+
+
+# Results 
+
+![alt_text](imgs/SAC_discrete_CP.png)
---
+++ agent.py
@@ -0,0 +1,162 @@
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+from torch.nn.utils import clip_grad_norm_
+from networks import Critic, Actor
+import copy
+
+
+class SAC(nn.Module):
+    """Interacts with and learns from the environment."""
+    
+    def __init__(self,
+                        state_size,
+                        action_size,
+                        device
+                ):
+        """Initialize an Agent object.
+        
+        Params
+        ======
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            random_seed (int): random seed
+        """
+        super(SAC, self).__init__()
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.device = device
+        
+        self.gamma = 0.99
+        self.tau = 1e-2
+        hidden_size = 256
+        learning_rate = 5e-4
+        self.clip_grad_param = 1
+
+        self.target_entropy = -action_size  # -dim(A)
+
+        self.log_alpha = torch.tensor([0.0], requires_grad=True)
+        self.alpha = self.log_alpha.exp().detach()
+        self.alpha_optimizer = optim.Adam(params=[self.log_alpha], lr=learning_rate) 
+                
+        # Actor Network 
+
+        self.actor_local = Actor(state_size, action_size, hidden_size).to(device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=learning_rate)     
+        
+        # Critic Network (w/ Target Network)
+
+        self.critic1 = Critic(state_size, action_size, hidden_size, 2).to(device)
+        self.critic2 = Critic(state_size, action_size, hidden_size, 1).to(device)
+        
+        assert self.critic1.parameters() != self.critic2.parameters()
+        
+        self.critic1_target = Critic(state_size, action_size, hidden_size).to(device)
+        self.critic1_target.load_state_dict(self.critic1.state_dict())
+
+        self.critic2_target = Critic(state_size, action_size, hidden_size).to(device)
+        self.critic2_target.load_state_dict(self.critic2.state_dict())
+
+        self.critic1_optimizer = optim.Adam(self.critic1.parameters(), lr=learning_rate)
+        self.critic2_optimizer = optim.Adam(self.critic2.parameters(), lr=learning_rate) 
+
+    
+    def get_action(self, state):
+        """Returns actions for given state as per current policy."""
+        state = torch.from_numpy(state).float().to(self.device)
+        
+        with torch.no_grad():
+            action = self.actor_local.get_det_action(state)
+        return action.numpy()
+
+    def calc_policy_loss(self, states, alpha):
+        _, action_probs, log_pis = self.actor_local.evaluate(states)
+
+        q1 = self.critic1(states)   
+        q2 = self.critic2(states)
+        min_Q = torch.min(q1,q2)
+        actor_loss = (action_probs * (alpha * log_pis - min_Q )).sum(1).mean()
+        log_action_pi = torch.sum(log_pis * action_probs, dim=1)
+        return actor_loss, log_action_pi
+    
+    def learn(self, step, experiences, gamma, d=1):
+        """Updates actor, critics and entropy_alpha parameters using given batch of experience tuples.
+        Q_targets = r + γ * (min_critic_target(next_state, actor_target(next_state)) - α *log_pi(next_action|next_state))
+        Critic_loss = MSE(Q, Q_target)
+        Actor_loss = α * log_pi(a|s) - Q(s,a)
+        where:
+            actor_target(state) -> action
+            critic_target(state, action) -> Q-value
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        # ---------------------------- update actor ---------------------------- #
+        current_alpha = copy.deepcopy(self.alpha)
+        actor_loss, log_pis = self.calc_policy_loss(states, current_alpha.to(self.device))
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+        
+        # Compute alpha loss
+        alpha_loss = - (self.log_alpha.exp() * (log_pis.cpu() + self.target_entropy).detach().cpu()).mean()
+        self.alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.alpha_optimizer.step()
+        self.alpha = self.log_alpha.exp().detach()
+
+        # ---------------------------- update critic ---------------------------- #
+        # Get predicted next-state actions and Q values from target models
+        with torch.no_grad():
+            _, action_probs, log_pis = self.actor_local.evaluate(next_states)
+            Q_target1_next = self.critic1_target(next_states)
+            Q_target2_next = self.critic2_target(next_states)
+            Q_target_next = action_probs * (torch.min(Q_target1_next, Q_target2_next) - self.alpha.to(self.device) * log_pis)
+
+            # Compute Q targets for current states (y_i)
+            Q_targets = rewards + (gamma * (1 - dones) * Q_target_next.sum(dim=1).unsqueeze(-1)) 
+
+
+        # Compute critic loss
+        q1 = self.critic1(states).gather(1, actions.long())
+        q2 = self.critic2(states).gather(1, actions.long())
+        
+        
+        critic1_loss = 0.5 * F.mse_loss(q1, Q_targets)
+        critic2_loss = 0.5 * F.mse_loss(q2, Q_targets)
+
+        
+        # Update critics
+        # critic 1
+        self.critic1_optimizer.zero_grad()
+        critic1_loss.backward(retain_graph=True)
+        clip_grad_norm_(self.critic1.parameters(), self.clip_grad_param)
+        self.critic1_optimizer.step()
+        # critic 2
+        self.critic2_optimizer.zero_grad()
+        critic2_loss.backward()
+        clip_grad_norm_(self.critic2.parameters(), self.clip_grad_param)
+        self.critic2_optimizer.step()
+
+        # ----------------------- update target networks ----------------------- #
+        self.soft_update(self.critic1, self.critic1_target)
+        self.soft_update(self.critic2, self.critic2_target)
+        
+        return actor_loss.item(), alpha_loss.item(), critic1_loss.item(), critic2_loss.item(), current_alpha
+
+    def soft_update(self, local_model , target_model):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        Params
+        ======
+            local_model: PyTorch model (weights will be copied from)
+            target_model: PyTorch model (weights will be copied to)
+            tau (float): interpolation parameter 
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(self.tau*local_param.data + (1.0-self.tau)*target_param.data)
